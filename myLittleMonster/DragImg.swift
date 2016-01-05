0a704ea8bb3ca389a920f613fb08fcdcc06130dc
Mehmet Ragıp Altuncu <mehmetaltuncu@gmail.com>
2016-01-04 21:14:47 -0800

    game done

---
+++ myLittleMonster/DragImg.swift
@@ -0,0 +1,51 @@
+//
+//  DragImg.swift
+//  myLittleMonster
+//
+//  Created by Mehmet Ragıp Altuncu on 03/01/16.
+//  Copyright © 2016 MehmetAltuncu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DragImg: UIImageView {
+    
+    var originalPosition: CGPoint!
+    var dropTarget: UIView?
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        originalPosition = self.center
+    }
+
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let position  = touch.locationInView(self.superview)
+            self.center = CGPointMake(position.x,position.y)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if let touch = touches.first, let target = dropTarget {
+            let position = touch.locationInView(self.superview)
+            
+            if CGRectContainsPoint(target.frame, position) {
+                NSNotificationCenter.defaultCenter().postNotificationName("onTargetDropped", object: nil)
+                
+            }
+            
+        }
+        
+        self.center = originalPosition
+        
+    }
+}
